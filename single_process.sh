059eb1e20348224c7a2e0f9dfba360dae36b6b47
Fr13ndSDP <2583067591@qq.com>
2023-10-23 19:41:11 +0800

    添加HPC运行脚本
    对进程绑定进行了修改,使用mpi --bind-to core. 原来写法在不独占节点情况下有可能绑定失败

---
+++ single_process.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+lrank=$OMPI_COMM_WORLD_LOCAL_RANK
+
+case ${lrank} in
+[0])
+  export HIP_VISIBLE_DEVICES=0
+  export UCX_NET_DEVICES=mlx5_0:1
+  export UCX_IB_PCI_BW=mlx5_0:50Gbs
+  numactl --membind=0 $@
+  ;;
+[1])
+  export HIP_VISIBLE_DEVICES=1
+  export UCX_NET_DEVICES=mlx5_1:1
+  export UCX_IB_PCI_BW=mlx5_1:50Gbs
+  numactl --membind=1 $@
+  ;;
+[2])
+  export HIP_VISIBLE_DEVICES=2
+  export UCX_NET_DEVICES=mlx5_2:1
+  export UCX_IB_PCI_BW=mlx5_2:50Gbs
+  numactl --membind=2 $@
+  ;;
+[3])
+  export HIP_VISIBLE_DEVICES=3
+  export UCX_NET_DEVICES=mlx5_3:1
+  export UCX_IB_PCI_BW=mlx5_3:50Gbs
+  numactl --membind=3 $@
+  ;;
+esac
