43912c0aaa6aa8ad06203a440e858c7b9713c207
Fr13ndSDP <2583067591@qq.com>
2023-10-23 19:42:10 +0800

    修改了环境加载以及make/make clean

--- env.sh
+++ env.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+module purge
 module load compiler/devtoolset/7.3.1 
-module load compiler/rocm/2.9.0
+module load compiler/rocm/2.9
 module load mpi/hpcx/2.4.1/gcc-7.3.1
 module list
